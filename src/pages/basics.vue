7cfdb8bdb751e541ac19287dce9737a7594bac8e
黄斌 <>
2019-05-06 18:19:53 +0800

    s

---
+++ src/pages/basics.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="fillcontain">
+      basics
+      <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    };
+  }
+};
+</script>
+
+<style lang="less">
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -13,6 +13,7 @@ Vue.use(Router)
 console.log(require('@/pages/home'))
 
 import Home from '@/pages/home'
+import basics from '@/pages/basics'
 import basics_html from '@/pages/basics/html'
 import basics_css from '@/pages/basics/css'
 import basics_js from '@/pages/basics/js'
@@ -23,37 +24,25 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
-    },{
-      path: '/basics/html/',
-      name: 'html',
-      component: basics_html
-    },{
-      path: '/basics/css/',
-      name: 'css',
-      component: basics_css
-    },{
-      path: '/basics/js/',
-      name: 'js',
-      component: basics_js
-    }
-  //   ,
-  //   {
-  //     path:'/basics/',
-  //     name:"basics",
-  //     children:[{
-  //       path: '/basics/html/',
-  //       name: 'html',
-  //       component: basics_html
-  //     },{
-  //       path: '/basics/css/',
-  //       name: 'css',
-  //       component: basics_css
-  //     },{
-  //       path: '/basics/js/',
-  //       name: 'js',
-  //       component: basics_js
-  //     }]
-  // }
+    },
+    {
+      path:'/basics/',
+      name:"basics",
+      component: basics,
+      children:[{
+        path: '/basics/html/',
+        name: 'html',
+        component: basics_html
+      },{
+        path: '/basics/css/',
+        name: 'css',
+        component: basics_css
+      },{
+        path: '/basics/js/',
+        name: 'js',
+        component: basics_js
+      }]
+  }
     // ...basics,
     // ...error
   ]
